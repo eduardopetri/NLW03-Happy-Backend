5ef2267daf44277720c8d03e5b7815f664aa0943
eduardopetri <eduardoafonsopetri@gmail.com>
2020-10-17 22:47:19 -0300

    Done backend

---
+++ src/views/ImagesView.ts
@@ -0,0 +1,13 @@
+import Image from "../models/Image";
+export default {
+  render(image: Image) {
+    return {
+      id: image.id,
+      path: `http://192.168.1.12:3333/uploads/${image.path}`,
+    };
+  },
+
+  renderMany(images: Image[]) {
+    return images.map(image=> this.render(image))
+  }
+}
